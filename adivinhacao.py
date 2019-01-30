f0c5a4b700a53d9bc1c69576c057d190236e35a1
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-30 20:59:54 -0200

    utilizando break e continue

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,13 +9,18 @@ total_de_tentativas = 3
 for rodada in range(0, total_de_tentativas):
     print("\n## Rodada ({}/{}) ##".format(rodada +1, total_de_tentativas))
 
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 0 e 100: ")
     print("Você digitou: ", chute_str)
 
     chute = int(chute_str)
 
+    if(chute < 0 or chute > 100):
+        print("Número inválido")
+        continue
+
     if (chute == numero_secreto):
         print("Você acertou!!")
+        break
     elif(chute > numero_secreto):
         print("Você errou!! Seu chute foi maior que o número secreto")
     else:
