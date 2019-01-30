8561b0c3c125760157f4d2ac16cd38daae5f471b
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-01-30 20:53:25 -0200

    utilizando for loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,10 +5,9 @@ print("*********************************")
 numero_secreto = 42
 
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
-    print("\n## Rodada ({}/{}) ##".format(rodada, total_de_tentativas))
+for rodada in range(0, total_de_tentativas):
+    print("\n## Rodada ({}/{}) ##".format(rodada +1, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
@@ -21,7 +20,5 @@ while(rodada <= total_de_tentativas):
         print("Você errou!! Seu chute foi maior que o número secreto")
     else:
         print("Você errou!! Seu chute foi menor que o número secreto")
-
-    rodada = rodada +1
 
 print("Fim do jogo.")
