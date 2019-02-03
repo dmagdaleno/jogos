a348d36d86aead966754eae65677dfa6ca49430a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-02-03 00:12:08 -0200

    coloca codigo dentro de função para poder modularizar

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,48 +1,53 @@
 import random
 
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
+def jogar():
 
-numero_secreto = round(random.random() *100)
-total_de_tentativas = 0
-pontos = 1000
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-print("\nEscolha um nível.")
-nivel = int(input("\n(1) Fácil, (2) Médio ou (3) Difícil: "))
+    numero_secreto = round(random.random() *100)
+    total_de_tentativas = 0
+    pontos = 1000
 
-if(nivel == 1):
-    total_de_tentativas = 15
-elif(nivel == 2):
-    total_de_tentativas = 10
-else:
-    total_de_tentativas = 3
+    print("\nEscolha um nível.")
+    nivel = int(input("\n(1) Fácil, (2) Médio ou (3) Difícil: "))
 
-for rodada in range(0, total_de_tentativas):
-    print("\n## Rodada ({}/{}) ##".format(rodada +1, total_de_tentativas))
-
-    chute_str = input("Digite um número entre 0 e 100: ")
-    print("Você digitou: ", chute_str)
-
-    chute = int(chute_str)
-
-    if(chute < 0 or chute > 100):
-        print("Número inválido")
-        continue
-
-    if (chute == numero_secreto):
-        print("Você acertou!!")
-        break
-    elif(chute > numero_secreto):
-        print("Você errou!! Seu chute foi maior que o número secreto")
+    if(nivel == 1):
+        total_de_tentativas = 15
+    elif(nivel == 2):
+        total_de_tentativas = 10
     else:
-        print("Você errou!! Seu chute foi menor que o número secreto")
+        total_de_tentativas = 3
 
-    delta = abs(numero_secreto - chute)
+    for rodada in range(0, total_de_tentativas):
+        print("\n## Rodada ({}/{}) ##".format(rodada +1, total_de_tentativas))
 
-    if(nivel == 3):
-        delta = delta *2
-    
-    pontos = pontos - delta
+        chute_str = input("Digite um número entre 0 e 100: ")
+        print("Você digitou: ", chute_str)
 
-print("Fim do jogo. Você marcou {} pontos!".format(pontos))
+        chute = int(chute_str)
+
+        if(chute < 0 or chute > 100):
+            print("Número inválido")
+            continue
+
+        if (chute == numero_secreto):
+            print("Você acertou!!")
+            break
+        elif(chute > numero_secreto):
+            print("Você errou!! Seu chute foi maior que o número secreto")
+        else:
+            print("Você errou!! Seu chute foi menor que o número secreto")
+
+        delta = abs(numero_secreto - chute)
+
+        if(nivel == 3):
+            delta = delta *2
+        
+        pontos = pontos - delta
+
+    print("Fim do jogo. Você marcou {} pontos!".format(pontos))
+
+if(__name__ == "__main__"):
+    jogar()
